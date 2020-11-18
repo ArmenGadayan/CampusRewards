8e0d1731eb56520fc005cef214d3dd42f0e017bf
Joseph <joeykahale23@gmail.com>
2020-11-18 11:24:02 -0800

    Removed Theme Title Banner

---
+++ app/src/main/java/com/example/majorx/SplashScreen.java
@@ -0,0 +1,12 @@
+package com.example.majorx;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+
+    }
+}
